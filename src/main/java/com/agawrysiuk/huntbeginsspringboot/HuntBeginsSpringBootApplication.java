a7e3892cd195c475376dbebe7eb68532ff99ae68
dlrzz <arkad.gawry@gmail.com>
2019-11-25 22:13:11 +0100

    Initial commit

---
+++ src/main/java/com/agawrysiuk/huntbeginsspringboot/HuntBeginsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.agawrysiuk.huntbeginsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntBeginsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuntBeginsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agawrysiuk/huntbeginsspringboot/HuntBeginsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agawrysiuk.huntbeginsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuntBeginsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
