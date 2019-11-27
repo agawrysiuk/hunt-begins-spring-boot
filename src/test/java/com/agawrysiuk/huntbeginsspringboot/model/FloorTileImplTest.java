00b6bc325207c0f1918c35df3101f90af5a661ad
dlrzz <arkad.gawry@gmail.com>
2019-11-27 19:13:38 +0100

    Added ParameterizedTests for rotate()

--- src/test/java/com/agawrysiuk/huntbeginsspringboot/model/FloorTileImplTest.java
+++ src/test/java/com/agawrysiuk/huntbeginsspringboot/model/FloorTileImplTest.java
@@ -1,20 +1,34 @@
 package com.agawrysiuk.huntbeginsspringboot.model;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.agawrysiuk.huntbeginsspringboot.model.Exit.*;
 
 class FloorTileImplTest {
 
-    @Test
-    void rotate_NewArrayIsCorrect() {
+    @ParameterizedTest
+    @MethodSource("provideArraysForRotate")
+    void rotate_NewArrayIsCorrect(int[] setExitsArray, Exit[] afterRotateArray) {
         FloorTile floorTile = new FloorTileImpl(1, "test");
-        floorTile.setExits(1, 0, 1, 1);
+        floorTile.setExits(setExitsArray);
         floorTile.rotate();
-        Assertions.assertArrayEquals(
-                new Exit[]{TOP_EXIT, RIGHT_EXIT, null, LEFT_EXIT},
-                floorTile.getExits()
+        Assertions.assertArrayEquals(afterRotateArray,floorTile.getExits()
+        );
+    }
+
+    private static Stream<Arguments> provideArraysForRotate() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 0, 1, 1}, new Exit[]{TOP_EXIT, RIGHT_EXIT, null, LEFT_EXIT}),
+                Arguments.of(new int[]{1, 1, 1, 1}, new Exit[]{TOP_EXIT, RIGHT_EXIT, BOT_EXIT, LEFT_EXIT}),
+                Arguments.of(new int[]{0, 0, 0, 0}, new Exit[]{null, null, null, null}),
+                Arguments.of(new int[]{0, 1, 0, 1}, new Exit[]{TOP_EXIT, null, BOT_EXIT, null}),
+                Arguments.of(new int[]{0, 0, 0, 1}, new Exit[]{TOP_EXIT, null, null, null}),
+                Arguments.of(new int[]{1, 0, 1, 1}, new Exit[]{TOP_EXIT, RIGHT_EXIT, null, LEFT_EXIT})
         );
     }
 }
