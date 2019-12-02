e5f443e2dd02c8e9c5097dd540a33e46306bf653
dlrzz <arkad.gawry@gmail.com>
2019-12-02 20:58:10 +0100

    Ready to implement map to the web

--- src/main/java/com/agawrysiuk/huntbeginsspringboot/web/controller/MainController.java
+++ src/main/java/com/agawrysiuk/huntbeginsspringboot/web/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.agawrysiuk.huntbeginsspringboot.web.controller;
 
+import com.agawrysiuk.huntbeginsspringboot.model.GameMap;
 import com.agawrysiuk.huntbeginsspringboot.service.GameService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
@@ -14,7 +17,21 @@ public class MainController {
     }
 
     @RequestMapping("/")
-    private String getHome() {
+    public String getHome() {
         return "home";
     }
+
+    @RequestMapping(value = "/", params = "create", method = RequestMethod.POST)
+    public String getMap(Model model) {
+        GameMap gameMap = gameService.getMap();
+        if (gameMap != null) {
+            model.addAttribute("map",gameMap);
+            return "map";
+        } else {
+            model.addAttribute("message", "Something went wrong while creating a map.");
+            return "home";
+        }
+
+    }
+
 }
