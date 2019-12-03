066a0e7b533c8588b4b983a7c8240ff284d45506
dlrzz <arkad.gawry@gmail.com>
2019-12-03 10:37:16 +0100

    Improved viewing, changed GameMapDto logic

--- src/main/java/com/agawrysiuk/huntbeginsspringboot/service/GameMapDto.java
+++ src/main/java/com/agawrysiuk/huntbeginsspringboot/service/GameMapDto.java
@@ -1,22 +1,18 @@
 package com.agawrysiuk.huntbeginsspringboot.service;
 
 import com.agawrysiuk.huntbeginsspringboot.model.FloorTile;
-import com.agawrysiuk.huntbeginsspringboot.model.GameMap;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+@Document
 public class GameMapDto {
     @Getter
+    @Setter
     private List<List<FloorTile>> gameListMap;
 
-    public GameMapDto(GameMap gameMap) {
-        gameListMap = new ArrayList<>();
-        for (FloorTile[] row : gameMap.getGameMap()) {
-            List<FloorTile> list = new ArrayList<>(Arrays.asList(row));
-            gameListMap.add(list);
-        }
+    public GameMapDto() {
     }
 }
--- src/main/java/com/agawrysiuk/huntbeginsspringboot/service/GameServiceImpl.java
+++ src/main/java/com/agawrysiuk/huntbeginsspringboot/service/GameServiceImpl.java
@@ -2,9 +2,13 @@ package com.agawrysiuk.huntbeginsspringboot.service;
 
 import com.agawrysiuk.huntbeginsspringboot.model.FloorTile;
 import com.agawrysiuk.huntbeginsspringboot.model.GameManager;
+import com.agawrysiuk.huntbeginsspringboot.model.GameMap;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -22,9 +26,20 @@ public class GameServiceImpl implements GameService {
             for (FloorTile floorTile : gameManager.getTiles()) {
                 floorTile.setImgPath("/tiles/"+floorTile.getName()+".png");
             }
-            return new GameMapDto(gameManager.createMap());
+            return transferMap(gameManager.createMap());
         } catch (IOException e) {
             return null;
         }
+    }
+
+    private GameMapDto transferMap(GameMap gameMap) {
+        List<List<FloorTile>> gameListMap = new ArrayList<>();
+        for (FloorTile[] row : gameMap.getGameMap()) {
+            List<FloorTile> list = new ArrayList<>(Arrays.asList(row));
+            gameListMap.add(list);
+        }
+        GameMapDto gameMapDto = new GameMapDto();
+        gameMapDto.setGameListMap(gameListMap);
+        return gameMapDto;
     }
 }
